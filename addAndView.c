743bf9f8c2aea86f64daba4fc323a9955df5ecfe
ShipraDimri <shipradimri18@gmail.com>
2026-03-31 19:12:19 +0530

    add and view function

--- addAndView.c
+++ addAndView.c
@@ -4,6 +4,7 @@ extern int count;
 void addTask() {
     list = realloc(list, (count + 1) * sizeof(struct Task));
     
+    // Ye line sabse zaroori hai: ID assign karna
     list[count].ID = count + 1; 
 
     printf("Task Name: ");
@@ -16,6 +17,7 @@ void addTask() {
     count++;
     printf("Task Added Successfully with ID: %d\n", list[count-1].ID);
 }
+
 void viewTask() {
     if (count == 0) {
         printf("Empty!\n");
@@ -23,7 +25,7 @@ void viewTask() {
     }
     printf("\n--- YOUR TASKS ---\n");
     for (int i = 0; i < count; i++) {
-        // Status logic: 0 hai toh Pending, 1 hai toh Completed
+       
         char statusText[15];
         if (list[i].status == 0) {
             strcpy(statusText, "Pending");
